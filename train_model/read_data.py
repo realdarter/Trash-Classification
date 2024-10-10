f0fa5152ac7f40007334b7978b2d9f767a322207
dartiros <100169417+realdarter@users.noreply.github.com>
2024-10-09 19:58:41 -0700

    training setup
    Setting up training model files

---
+++ train_model/read_data.py
@@ -0,0 +1,9 @@
+import os
+print(os.getcwd())  # This will show the current working directory
+
+
+#with open('paths.txt', 'r') as file:
+#    paths = file.readlines()
+
+#print(paths)
+    #key, value = line.split('=')
