5af20a888a613634ba69951eabbd168ea14f85c6
dartiros <100169417+realdarter@users.noreply.github.com>
2024-10-09 20:21:53 -0700

    Gitignore Update to ignore all Massive Data
    Ignoring Data

--- train_model/read_data.py
+++ train_model/read_data.py
@@ -1,9 +1,30 @@
 import os
-print(os.getcwd())  # This will show the current working directory
+base_dir = "/data/garbagedata"
+images_dict = {}
+image_extension = {'.jpg', '.jpeg', '.png'}
+
+folder_name = os.path.basename(base_dir)
+print(folder_name)
+
+for root, dirs, files in os.walk(base_dir):
+    folder_name = os.path.basename(root)
 
 
-#with open('paths.txt', 'r') as file:
-#    paths = file.readlines()
+"""
+
+try:
+    with open('paths.txt', 'r') as file:
+        file_lines = file.readlines()
+except Exception as x:
+    print(x)
+
 
 #print(paths)
-    #key, value = line.split('=')
+
+for index, line in enumerate(file_lines):
+    key, value = line.split('=')
+    if("path" == key):
+        current_path = value
+        print(current_path)
+
+"""
---
+++ train_model/train_model.py
@@ -0,0 +1,6 @@
+import pandas as pd
+import os
+
+
+
+garbage_data = []
