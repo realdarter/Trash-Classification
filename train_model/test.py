4175462b86f5e1cc6aefb681b53ded091c04e25b
dartiros <danielbao.real@gmail.com>
2024-11-06 21:58:09 -0800

    Update test.py

--- train_model/test.py
+++ train_model/test.py
@@ -2,10 +2,7 @@ from image_classifier import *
 
 # Example usage to predict an image
 image_path = r"C:\Users\minec\Downloads\test\metal.jpg"  # Replace with your image path
-image_tensor = preprocess_image(image_path)
 
-model, classes = load_model("checkpoint/save1")
 
-top_k_predictions = predict(model, image_tensor, classes, k=3)
-for class_name, prob in top_k_predictions:
-    print(f"Class {class_name}: {prob:.2f}%")
+test_image_path = r'C:\Users\minec\Downloads\test\metal.jpg'  # Replace with your image path
+    predict_top_k(test_image_path, model, transformations, args, device=device)
