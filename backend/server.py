c37f01890f4bf442f2daa0c4db5865f3ace7ee55
brownduck <147341472+brownduck@users.noreply.github.com>
2024-12-01 22:09:35 -0800

    Update server.py
    changes

--- backend/server.py
+++ backend/server.py
@@ -1,7 +1,7 @@
 from flask import Flask, render_template, request, jsonify
 import sys
 import os
-
+import test
 # Add the train_model folder to the Python module search path
 sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../train_model')))
 
@@ -16,11 +16,12 @@ def index():
 def takeImage():
     if 'image' not in request.files:
         return jsonify({"no image"}), 400
-
     image = request.files['image']
     # hopefully returns a tuple that includes the predicted classification and the percent.
     prediction = predictImage(image)
-    return prediction
+    print(prediction)
+    return "Prediction: " + str(prediction[0]) + "Confidence: " + str(prediction[1])
+    # return prediction
     # return "RAHHHH"
 
 if __name__ == '__main__':
