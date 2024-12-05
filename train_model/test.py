a1ecdc8bc8c52c908f60f1279cc125eeb34a0fc7
brownduck <147341472+brownduck@users.noreply.github.com>
2024-12-04 20:05:36 -0800

    Update test.py

--- train_model/test.py
+++ train_model/test.py
@@ -15,22 +15,17 @@ def choose_image():
 model_dir = 'saved_models/save1'
 
 args = create_args(
-        num_epochs=2, 
-        batch_size=32, 
-        learning_rate=1e-5, 
-        save_every=500, 
-        max_length=256, 
-        temperature=1.0, 
+        num_epochs=2,
+        batch_size=32,
+        learning_rate=1e-5,
+        save_every=500,
+        max_length=256,
+        temperature=1.0,
         top_k=1, #adjust for the top classified types
-        top_p=0.9, 
+        top_p=0.9,
         repetition_penalty=1.0
         )
 # basically using the while loop. May not work if image path is different from the image itself.
 def predictImage(image):
     return predict_images(image, model=model, classes=classes, args=args)
 model, epoch, classes = load_model(model_dir)
-while True:
-    #test_image_path = r'C:\Users\minec\Downloads\test\metal.jpg'
-    test_image_path = choose_image()
-    top_k_labels, top_k_probs = predict_images(img_path=test_image_path, model=model, classes=classes, args=args)
-    print(top_k_labels, top_k_probs)
