ee21375a29d927969e960d72c272c25e800d1a3c
brownduck <147341472+brownduck@users.noreply.github.com>
2024-12-07 21:12:03 -0800

    Add files via upload
    trashThingBackend

--- backend/main.py
+++ backend/main.py
@@ -1,11 +1,5 @@
 from flask import Flask, render_template, request, jsonify
-import sys
-import os
 import test
-# Add the train_model folder to the Python module search path
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../train_model')))
-
-from test import predictImage  # Import predictImage from test.py
 app = Flask(__name__)
 
 @app.route('/')
@@ -16,13 +10,12 @@ def index():
 def takeImage():
     if 'image' not in request.files:
         return jsonify({"no image"}), 400
+
     image = request.files['image']
     # hopefully returns a tuple that includes the predicted classification and the percent.
-    prediction = predictImage(image)
-    print(prediction)
-    return "Prediction: " + str(prediction[0]) + "Confidence: " + str(prediction[1])
+    # prediction = test.predictImage(image)
     # return prediction
-    # return "RAHHHH"
+    return "RAHHHH"
 
 if __name__ == '__main__':
     app.run(debug=True)
